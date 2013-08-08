db0ccb137fbb417a56fc46aad29ea62ca4856ffe
Freek Paans <freekpaans@gmail.com>
2013-08-08 16:16:06 +0200

    added note to say we switch quickselect implementation

--- PagePerformanceInsights/Handler/Algorithms/Median/QuickSelect.cs
+++ PagePerformanceInsights/Handler/Algorithms/Median/QuickSelect.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 
 namespace PagePerformanceInsights.Handler.Algorithms.Median {
+	//from: http://blog.teamleadnet.com/2012/07/quick-select-algorithm-find-kth-element.html
+	//tried to implement a recursion version from wikipedia first, got stackoverflow exceptions and this one seems to work..
 	class QuickSelect2 {
 		public static int selectKth(int[] arr,int k) {
 			if(arr == null || arr.Length <= k)
@@ -60,52 +62,52 @@ namespace PagePerformanceInsights.Handler.Algorithms.Median {
 
 			return QuickSelect2.selectKth(list,k);
 
-			return Select(list,0,list.Length-1,k);
+			//return Select(list,0,list.Length-1,k);
 		}
-		int Select(int[] list,int left,int right,int k) {
+		//int Select(int[] list,int left,int right,int k) {
 			
-			if(left==right) {
-				return list[left];
-			}
-			var pivotIndex = rnd.Next(left,right);
-			var pivotNewIndex = Partition(list,left,right,pivotIndex);
-			var pivotDist = pivotNewIndex-left+1;
+		//	if(left==right) {
+		//		return list[left];
+		//	}
+		//	var pivotIndex = rnd.Next(left,right);
+		//	var pivotNewIndex = Partition(list,left,right,pivotIndex);
+		//	var pivotDist = pivotNewIndex-left+1;
 
-			if(pivotDist == k) {
-				return list[pivotNewIndex];
-			}
-			if(k<pivotDist) {
-				return Select(list,left,pivotNewIndex-1,k);
-			}
-			return Select(list,pivotNewIndex+1,right,k-pivotDist);
-		}
+		//	if(pivotDist == k) {
+		//		return list[pivotNewIndex];
+		//	}
+		//	if(k<pivotDist) {
+		//		return Select(list,left,pivotNewIndex-1,k);
+		//	}
+		//	return Select(list,pivotNewIndex+1,right,k-pivotDist);
+		//}
 
-		Random rnd = new Random();
+		//Random rnd = new Random();
 
-		int Partition(int[] items,int left,int right,int pivotIndex) {
-			var pivotValue = items[pivotIndex];
-			Swap(items,pivotIndex,right);
-			var storeIndex = left;
+		//int Partition(int[] items,int left,int right,int pivotIndex) {
+		//	var pivotValue = items[pivotIndex];
+		//	Swap(items,pivotIndex,right);
+		//	var storeIndex = left;
 
-			for(var i=left;i<right;i++) {
-				if(items[i]>=pivotValue) {
-					continue;
-				}
-				Swap(items,i,storeIndex);
-				storeIndex++;
-			}
-			Swap(items,right,storeIndex);
-			return storeIndex;
-		}
+		//	for(var i=left;i<right;i++) {
+		//		if(items[i]>=pivotValue) {
+		//			continue;
+		//		}
+		//		Swap(items,i,storeIndex);
+		//		storeIndex++;
+		//	}
+		//	Swap(items,right,storeIndex);
+		//	return storeIndex;
+		//}
 
-		private void Swap(int[] items,int idx1,int idx2) {
-			var tmp = items[idx1];
-			items[idx1] = items[idx2];
-			items[idx2] = tmp;
-		}
+		//private void Swap(int[] items,int idx1,int idx2) {
+		//	var tmp = items[idx1];
+		//	items[idx1] = items[idx2];
+		//	items[idx2] = tmp;
+		//}
 
-		public static int Median(List<int> list) {
-			return new QuickSelect().Select(list.ToArray(), 0.5);
-		}
+		//public static int Median(List<int> list) {
+		//	return new QuickSelect().Select(list.ToArray(), 0.5);
+		//}
 	}
 }
