ac2beaf80d019195df7af329f224fe9f83904434
Freek Paans <freekpaans@gmail.com>
2013-08-12 15:37:02 +0200

    fixed bug with sha1 returning 00000...

--- PagePerformanceInsights.SqlServerStore/Helpers/SHA1Helper.cs
+++ PagePerformanceInsights.SqlServerStore/Helpers/SHA1Helper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -6,16 +7,16 @@ using System.Text;
 
 namespace PagePerformanceInsights.SqlServerStore.Helpers {
 	static class SHA1Helper {
-		readonly static Dictionary<string,string> _SHA1Cache = new Dictionary<string,string>();
+		readonly static ConcurrentDictionary<string,string> _SHA1Cache = new ConcurrentDictionary<string,string>();
 
-		readonly static SHA1 _sha1 = SHA1.Create();
+		//readonly static SHA1 _sha1 = SHA1.Create();
 
 		public static string GetSHA1String(string input) {
 			if(!_SHA1Cache.ContainsKey(input)) {
-				var bytes = _sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
-				lock(_SHA1Cache) {
-					_SHA1Cache[input] = BitConverter.ToString(bytes).Replace("-","");
-				}
+				var bytes = SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(input));
+				var value = BitConverter.ToString(bytes).Replace("-","");
+
+				_SHA1Cache[input] = value;
 			}
 			return _SHA1Cache[input];
 		}
