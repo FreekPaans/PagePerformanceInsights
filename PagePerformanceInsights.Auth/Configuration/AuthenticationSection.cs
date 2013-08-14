fe872a4bcb09002cd10c9d7933c96a67e7b28809
Freek Paans <freekpaans@gmail.com>
2013-08-14 16:29:16 +0200

    added some custom auth, pbb needs review

---
+++ PagePerformanceInsights.Auth/Configuration/AuthenticationSection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace PagePerformanceInsights.Configuration {
+	public class AuthenticationSection : ConfigurationSection {
+		[ConfigurationProperty("PPIPath",IsRequired=true)]
+		public string PPIPath{
+			get {
+				return (string)this["PPIPath"];
+			}
+			set {
+				this["PPIPath"] = value;
+			}
+		}
+
+		[ConfigurationProperty("principalUsername",IsRequired=true)]
+		public string PrincipalUsername {
+			get {
+				return (string)this["principalUsername"];
+			}
+			set {
+				this["principalUsername"] = value;
+			}
+		}
+
+		[ConfigurationProperty("",IsDefaultCollection=true)]
+		public AllowCollection IPs {
+			get {
+				return (AllowCollection)base[""];
+			}
+			set {
+				this[""]  = value;
+			}
+		}
+
+		public static AuthenticationSection Get() {
+			return (AuthenticationSection)(ConfigurationManager.GetSection("ppi/auth")??new AuthenticationSection());
+		}
+	}
+
+	[ConfigurationCollection(typeof(AllowElement))]
+	public class AllowCollection:ConfigurationElementCollection {
+		protected override ConfigurationElement CreateNewElement() {
+			return new AllowElement();
+		}
+		protected override object GetElementKey(ConfigurationElement element) {
+			return ((AllowElement)element).Name;
+		}
+	}
+
+	public class AllowElement:ConfigurationElement {
+		[ConfigurationProperty("name", IsRequired=true)]
+		public string Name {
+			get {
+				return (string)this["name"];
+			}
+			set {
+				this["name"]=value;
+			}
+		}
+
+		[ConfigurationProperty("ip",IsRequired=true)]
+		//todo: stringvalidator
+		public string IP {
+			get {
+				return (string)this["ip"];
+			}
+			set {
+				this["ip"] = value;
+			}
+		}
+	}
+}
---
+++ PagePerformanceInsights.Auth/Modules/PPIAuthModule.cs
@@ -0,0 +1,61 @@
+﻿using PagePerformanceInsights.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+
+namespace PagePerformanceInsights.Auth.Modules {
+	public class PPIAuthModule : IHttpModule{
+		public void Dispose() {
+			//throw new NotImplementedException();
+		}
+
+		readonly static string _principalUsername;
+		readonly static HashSet<string> _IPs;
+		readonly static string _PPIBasePath;
+		readonly static bool _customAuthEnabled;
+
+		static PPIAuthModule() {
+			var settings = PagePerformanceInsights.Configuration.AuthenticationSection.Get();
+
+			if(string.IsNullOrWhiteSpace(settings.PrincipalUsername) || string.IsNullOrWhiteSpace(settings.PPIPath)) {
+				_customAuthEnabled = false;
+				return;
+			}
+
+			_principalUsername = settings.PrincipalUsername;
+			_PPIBasePath = settings.PPIPath;
+			_IPs = new HashSet<string>(settings.IPs.Cast<AllowElement>().Where(a=>!string.IsNullOrWhiteSpace(a.IP)).Select(a=>a.IP));
+			_customAuthEnabled = true;
+
+		}
+
+		public void Init(HttpApplication context) {
+			if(!_customAuthEnabled) {
+				return;
+			}
+
+			context.AuthenticateRequest+=(s,e)=> {
+				if(!HttpContext.Current.Request.Url.LocalPath.StartsWith(_PPIBasePath)) {
+					return;
+				}
+
+				if(HttpContext.Current.Request.IsAuthenticated) {
+					return;
+				}
+
+				if(!_IPs.Contains(HttpContext.Current.Request.UserHostAddress)) {
+					return;
+				}
+
+				var principal = new GenericPrincipal(new GenericIdentity(_principalUsername),null);
+				Thread.CurrentPrincipal = principal;
+				HttpContext.Current.User = principal;
+				return;
+			};
+			//throw new NotImplementedException();
+		}
+	}
+}
